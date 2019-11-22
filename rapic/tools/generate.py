dd72f3d44539d3f961b9995518f7e00ad70b0f77
Yomi <yomi@erpsoftapp.com>
2019-11-22 01:57:28 +0100

    Removed redundant code from generator

--- rapic/tools/generate.py
+++ rapic/tools/generate.py
@@ -4,13 +4,13 @@ from .burp import create_endpoint
 
 
 def burp_request_files(client_name, burp_xml_files_loc):
-    requests = []
     total_page_reqs = 0
     request_load = {}
-    request_load['pages'] = [os.path.splitext(os.path.basename(file))[0] for file in burp_xml_files_loc]
+    pages = []
     for file in burp_xml_files_loc:
         with open(file) as e:
             page = os.path.splitext(os.path.basename(file))[0]
+            pages.append(page)
             request_items = xmltodict.parse(e.read())['items']
             if 'item' not in request_items:
                 continue
@@ -20,4 +20,5 @@ def burp_request_files(client_name, burp_xml_files_loc):
             total_page_reqs = total_page_reqs + endpoint['total_requests']
             request_load[page] = endpoint
     request_load['total_client_requests'] =  total_page_reqs
+    request_load['pages'] =  pages
     return {client_name: request_load}
